ac9fcac03c5168ffce59c902d6c3be48fa4123e7
jaryapp <jaryapp@gmail.com>
2021-03-09 01:24:48 +0900

    [#2] style: change px -> em, apply code review

--- src/components/Notice/Selector/style.ts
+++ src/components/Notice/Selector/style.ts
@@ -4,11 +4,12 @@ import '@src/assets/style/__pallette.css'
 export const SelectorWrapper = styled.ul`
   display: flex;
   overflow-x: auto;
-  padding: 5px;
+  padding: 0.5em;
   background-color: white;
+
   & > li {
     flex-shrink: 0;
-    margin: 5px;
+    margin: 0.3em;
     border-radius: 20px;
     padding: 0.3em 0.5em;
     box-shadow: rgba(149, 157, 165, 0.2) 0px 3px 9px;
