84ee55d47b6a12063228ec98ebefd39c75778f9c
jaryapp <jaryapp@gmail.com>
2021-03-09 01:11:32 +0900

    markup: basic notice selector

---
+++ src/components/Notice/Selector/style.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components'
+import '@src/assets/style/__pallette.css'
+
+export const SelectorWrapper = styled.ul`
+  display: flex;
+  overflow-x: auto;
+  padding: 5px;
+  background-color: white;
+  & > li {
+    flex-shrink: 0;
+    margin: 5px;
+    border-radius: 20px;
+    padding: 0.3em 0.5em;
+    box-shadow: rgba(149, 157, 165, 0.2) 0px 3px 9px;
+    color: var(--black-500);
+  }
+`
