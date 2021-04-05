f830d917b6b01eed43f795a38116c427ac9186b4
nohgijin <ynda11111@naver.com>
2021-04-05 17:59:19 +0900

    feat: loading component 생성

---
+++ src/components/shared/Loading/index.tsx
@@ -0,0 +1,11 @@
+import StyledLoading from './style'
+
+function Loading() {
+  return (
+    <StyledLoading>
+      <img src="/img/uang_loading.png" alt="로딩중" />
+    </StyledLoading>
+  )
+}
+
+export default Loading
